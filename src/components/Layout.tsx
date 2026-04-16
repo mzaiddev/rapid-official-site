94de4f69e8560758d111bd1d925d5feb8f3c912a
zaidkhan-code <zaidkhan1357913579@gmail.com>
2026-04-16 22:38:33 +0500

    added updated code about lining with scrool

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import { Menu, X, Facebook, Twitter, Linkedin, Instagram, Mail, Phone, MapPin } from 'lucide-react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { cn } from '../lib/utils';
 
 const Logo = () => (
@@ -16,6 +16,10 @@ const Logo = () => (
 export default function Layout() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   const navLinks = [
     { name: 'Home', path: '/' },
